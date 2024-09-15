fbf9e8f196eb382fa24bf05820616609622656df
Ekaterina Goncharenko <eg93006@gmail.com>
2024-09-15 06:51:39 +0300

    first commit

---
+++ Exchange.js
@@ -0,0 +1,28 @@
+const formHandlerRegister = new FormHandler("#reg_form");
+
+const base_url = "https://data.fixer.io/api/";
+const api_key = "b0563b6dc78ccfd17382d5b64ffc1845";
+
+const result_text = document.getElementById("result_text");
+
+async function exchangeFetch(data) {
+    try {
+        const response = await fetch(`${base_url}latest?access_key=${api_key}`
+            + "&symbols=" + data.from + "," + data.to);
+        const result = await response.json();
+        if (result.success) {
+            const firstSymbolRate = result.rates[data.from];
+            const secondSymbolRate = result.rates[data.to];
+
+            const res = data.amount * (secondSymbolRate / firstSymbolRate);
+            result_text.textContent = `Result = ${res}`;
+            result_text.style.display = "flex";
+        } else {
+            console.error(result.error);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+formHandlerRegister.addHandler(exchangeFetch);
---
+++ FormHandler.js
@@ -0,0 +1,32 @@
+class FormHandler {
+    constructor(selector) {
+        this.formElement = document.querySelector(selector)
+    }
+
+
+    addHandler = (func) => {
+        this.formElement.addEventListener('submit', (e) => {
+            e.preventDefault();
+            let objData = {};
+            console.log(this.formElement.elements)
+            const data = Array.from(this.formElement.elements)
+            console.log(data)
+            data.filter(item => item.name)
+                .map(element => {
+                    let name = element.name;// coffee |size
+                    let value = element.value;// latte   | large
+                    let checked = element.checked
+                    let type = element.type
+                    // const {name, value, checked, type} = element;
+                    if(type !== "radio")
+                        objData[name] = value;
+                    else if(checked)
+                        objData[name] = value;
+                })
+            console.log(objData)
+            let res = func(objData)
+            if(!res)
+                e.target.reset();
+        })
+    }
+}
